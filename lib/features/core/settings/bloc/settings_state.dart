6b1f97c4c14e9832bae2de6ecaf0624449ed8bf9
Drago Vrban <drago.vrban@jsguru.io>
2023-01-17 08:53:48 +0100

    upgraded template

---
+++ lib/features/core/settings/bloc/settings_state.dart
@@ -0,0 +1,10 @@
+part of 'settings_cubit.dart';
+
+abstract class SettingsState extends Equatable {
+  const SettingsState();
+}
+
+class SettingsInitial extends SettingsState {
+  @override
+  List<Object> get props => [];
+}
