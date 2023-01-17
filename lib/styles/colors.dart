7e72cba468a270596b8e0309ec3e44982b889ed2
Drago Vrban <drago.vrban@jsguru.io>
2023-01-17 09:03:26 +0100

    added custom theme

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -4,3 +4,15 @@ const primaryBlack = Colors.black;
 const primaryWhite = Colors.white;
 const darkBlue = Colors.blueAccent;
 const lightBlue = Colors.lightBlueAccent;
+
+class AppColors {
+  static const primaryColor = Color(0xFF0C0B0C);
+  static const secondaryColor = Color(0xFFFFFFFF);
+  static const bodyColor = Color(0xFFA4A8B2);
+  static const bodyColorLight = Color(0xFFE2E5ED);
+  static const bodyColorDark = Color(0xFF9FA9C0);
+  static const green = Color(0xFFC9F322);
+  static const mint = Color(0xFF2AAFB3);
+  static const blue = Color(0xFF6282F5);
+  static const red = Color(0xFFD32F2F);
+}
--- lib/styles/themes.dart
+++ lib/styles/themes.dart
@@ -2,6 +2,8 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'colors.dart';
+
 abstract class Themes {
   static ThemeData darkTheme = FlexThemeData.dark(
     scheme: FlexScheme.sakura,
@@ -45,4 +47,117 @@ abstract class Themes {
   //   TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
   //   TargetPlatform.android: CupertinoPageTransitionsBuilder(),
   // }));
+
+
+
+
+  // This is for custom theme
+
+  // static ThemeData darkTheme = ThemeData(
+  //   brightness: Brightness.dark,
+  //   primaryColor: darkBlue,
+  //   scaffoldBackgroundColor: lightBlue,
+  //   textTheme: const TextTheme(
+  //     bodyText1: TextStyle(
+  //       color: Colors.white,
+  //     ),
+  //   ),
+  // );
+  //
+  // static ThemeData lightTheme = ThemeData(
+  //   brightness: Brightness.light,
+  //   primaryColor: AppColors.secondaryColor,
+  //   scaffoldBackgroundColor: AppColors.secondaryColor,
+  //   backgroundColor: AppColors.primaryColor,
+  //   textTheme: const TextTheme(
+  //     headline1: TextStyle(
+  //       fontFamily: 'Rany',
+  //       fontSize: 36,
+  //       height: 1.2,
+  //       fontWeight: FontWeight.w500,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     headline2: TextStyle(
+  //       fontFamily: 'Rany',
+  //       fontSize: 30,
+  //       fontWeight: FontWeight.w500,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     headline3: TextStyle(
+  //       fontFamily: 'Rany',
+  //       fontSize: 24,
+  //       fontWeight: FontWeight.w600,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     headline4: TextStyle(
+  //       fontFamily: 'Rany',
+  //       fontSize: 20,
+  //       fontWeight: FontWeight.w500,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     headline5: TextStyle(
+  //       fontFamily: 'Poppins',
+  //       fontSize: 14,
+  //       fontWeight: FontWeight.w500,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     headline6: TextStyle(
+  //       fontFamily: 'Poppins',
+  //       fontSize: 12,
+  //       fontWeight: FontWeight.w500,
+  //       color: AppColors.primaryColor,
+  //     ),
+  //     bodyText1: TextStyle(
+  //         fontFamily: 'Poppins',
+  //         fontSize: 12,
+  //         fontWeight: FontWeight.w400,
+  //         color: AppColors.bodyColor,
+  //         height: 1.8),
+  //     bodyText2: TextStyle(
+  //       fontFamily: 'Poppins',
+  //       fontSize: 14,
+  //       height: 1.8,
+  //       fontWeight: FontWeight.w400,
+  //       color: AppColors.bodyColor,
+  //     ),
+  //   ),
+  //   inputDecorationTheme: const InputDecorationTheme(
+  //     border: OutlineInputBorder(
+  //       borderRadius: BorderRadius.all(Radius.circular(10)),
+  //       borderSide: BorderSide(
+  //         color: AppColors.secondaryColor,
+  //         style: BorderStyle.solid,
+  //         width: 1,
+  //       ),
+  //     ),
+  //     floatingLabelStyle: TextStyle(
+  //       color: AppColors.bodyColor,
+  //       fontWeight: FontWeight.w500,
+  //       fontSize: 12,
+  //       fontFamily: 'Poppins',
+  //     ),
+  //     hintStyle: TextStyle(
+  //       fontFamily: "Poppins",
+  //       fontWeight: FontWeight.w400,
+  //       fontSize: 14,
+  //       color: primaryBlack,
+  //     ),
+  //     labelStyle: TextStyle(
+  //       fontFamily: 'Poppins',
+  //       fontWeight: FontWeight.w400,
+  //       fontSize: 14,
+  //     ),
+  //     focusedBorder: OutlineInputBorder(
+  //       borderRadius: BorderRadius.all(Radius.circular(10)),
+  //       borderSide: BorderSide(
+  //         color: AppColors.bodyColorDark,
+  //         style: BorderStyle.solid,
+  //         width: 1,
+  //       ),
+  //     ),
+  //   ),
+  // );
+
+
+
 }
